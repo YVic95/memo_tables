af43bde62f8cc253234fa40e1651003cb9656592
YVic95 <vika.yevlentieva@gmail.com>
2026-07-16 18:38:49 +0300

    added InitialRuleState

--- graphs/states.py
+++ graphs/states.py
@@ -1,6 +1,21 @@
-from typing import TypedDict
+from typing import TypedDict, Optional
+from sqlalchemy.orm import Session
 
 class RuleCreationAgentState(TypedDict):
     native_language: str
     target_language: str
     proposed_rules: list[dict]
+
+class InitialRuleState(TypedDict):
+    db: Session
+    rule_title: str
+    rule_explanation: str
+    native_language: str
+    target_language: str
+    native_language_id: str
+    target_language_id: str
+    word_category_id: str
+    grammar_rule_id: str
+    translated_name: str
+    translated_description: str
+    full_content: Optional[str]
